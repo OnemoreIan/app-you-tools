59547d23c2ead87526cf2edaaa7d461a48ca4172
Ian <iandelrazo@gmail.com>
2024-10-29 22:45:20 -0600

    first update

---
+++ src/main/java/com/onemoreian/app_you_tools/App_you_tools.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.onemoreian.app_you_tools;
+
+/**
+ *
+ * @author Ian
+ */
+public class App_you_tools {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        // reqwuisitos interfaz dependencia JavaFX
+        //conexion a internet
+        //conexion a yutube
+        //credenciales del sitio web
+        //conversor de formatos
+        //empaquetador de archvios mp3 y mp4
+        
+    }
+}
